3a0482ac7703cce5825626946cfd3a4fccdb03a6
nphenley <nathenley@gmail.com>
2022-12-20 13:03:46 +0100

    small fixes

--- wookie-movies/components/Details.tsx
+++ wookie-movies/components/Details.tsx
@@ -11,8 +11,8 @@ type DetailProps = {
 export default function Details(props: DetailProps) {
   return (
     <div className="md:mr-8">
-      <div className="flex h-full mt-10 justify-center">
-        <div className="sm:w-2/5 w-0 md:mx-4  ">
+      <div className="flex justify-center h-full mt-10">
+        <div className="w-0 lg:w-1/4 sm:1/3 md:mx-4 ">
           <Image
             style={{ objectFit: "cover" }}
             unoptimized={true}
@@ -22,10 +22,10 @@ export default function Details(props: DetailProps) {
             alt={`${props.movie.title} poster.`}
           />
         </div>
-        <div className="sm:w-2/3 w-11/12 flex flex-col gap-4">
+        <div className="flex flex-col w-11/12 gap-4 sm:w-2/3">
           <div className="flex justify-between">
             <div className="flex gap-4">
-              <div className="font-bold text-3xl">{props.movie.title}</div>
+              <div className="text-3xl font-bold">{props.movie.title}</div>
               <div className="flex items-center">
                 {props.movie.imdb_rating} <FaImdb size={30} />
               </div>
@@ -46,7 +46,7 @@ export default function Details(props: DetailProps) {
               <div>{new Date(props.movie.released_on).getFullYear()}</div>|
               <div>{props.movie.length}</div>|
               <div>
-                {props.movie.director.length == 2
+                {Array.isArray(props.movie.director)
                   ? props.movie.director[0]
                   : props.movie.director}
               </div>
@@ -59,7 +59,7 @@ export default function Details(props: DetailProps) {
             </div>
           </div>
 
-          <div className="tracking-tightest text-2xl">
+          <div className="text-2xl tracking-tightest">
             {props.movie.overview}
           </div>
         </div>
